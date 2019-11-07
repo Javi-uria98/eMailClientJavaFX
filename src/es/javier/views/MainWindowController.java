dad467f21699e38259c21aa99f65e48198a95d45
Javi-uria98 <55594972+Javi-uria98@users.noreply.github.com>
2019-11-07 19:16:21 +0100

    Commit tras crear el objeto email

--- src/es/javier/views/MainWindowController.java
+++ src/es/javier/views/MainWindowController.java
@@ -3,6 +3,7 @@ package es.javier.views;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeView;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -12,7 +13,8 @@ public class MainWindowController  implements Initializable {
     @FXML
     private TableView tableViewEmail;
 
-
+    @FXML
+    private TreeView treeViewEmail;
 
 
 
