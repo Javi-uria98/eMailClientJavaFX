e8da5cbb60862723df9186b875834d5942542654
Javi-uria98 <55594972+Javi-uria98@users.noreply.github.com>
2019-11-07 19:10:08 +0100

    Commit tras crear el MainWindowController y algun cambio en el fxml

--- src/es/javier/views/MainWindowController.java
+++ src/es/javier/views/MainWindowController.java
@@ -1,4 +1,23 @@
 package es.javier.views;
 
-public class MainWindowController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MainWindowController  implements Initializable {
+
+    @FXML
+    private TableView tableViewEmail;
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
 }
