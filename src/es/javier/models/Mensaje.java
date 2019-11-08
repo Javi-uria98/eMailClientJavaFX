4ef80c0050cc45382ae464c91177fcfbbbae3ac3
Javi-Uria98 <javi_uria98@outlook.es>
2019-11-09 00:20:40 +0100

    Commit tras la creación de la clase Mensaje

---
+++ src/es/javier/models/Mensaje.java
@@ -0,0 +1,25 @@
+package es.javier.models;
+
+import javax.mail.*;
+import java.text.SimpleDateFormat;
+
+public class Mensaje {
+
+    private Message message;
+
+    public Mensaje(Message message){this.message = message;}
+
+    public String getRemitente() throws MessagingException {
+        return message.getFrom()[0].toString();
+    }
+
+    public String getAsunto() throws MessagingException{
+        return message.getSubject();
+    }
+
+    public String getFecha() throws MessagingException{
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yy");
+        return sdf.format(message.getReceivedDate());
+    }
+
+}
