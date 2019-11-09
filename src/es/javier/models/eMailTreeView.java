8389553f5d456263c072f52d9816ad4741c72c87
Javi-Uria98 <javi_uria98@outlook.es>
2019-11-09 04:36:03 +0100

    Commit tras la creación de la clase Mensaje

--- src/es/javier/models/eMailTreeView.java
+++ src/es/javier/models/eMailTreeView.java
@@ -2,16 +2,18 @@ package es.javier.models;
 
 import javafx.scene.control.TreeItem;
 
+import javax.mail.MessagingException;
+
 public class eMailTreeView {
 
     private final TreeItem<String> rootItem = new TreeItem();
 
-    public eMailTreeView (eMail  email){
+    public eMailTreeView (Mensaje  mensaje) throws MessagingException {
         rootItem.setValue("e-mail");
 
-        rootItem.getChildren().add(new TreeItem("Remitente: "+email.getRemitente()));
-        rootItem.getChildren().add(new TreeItem("Asunto: "+email.getAsunto()));
-        rootItem.getChildren().add(new TreeItem("Fecha: "+email.getFecha()));
+        rootItem.getChildren().add(new TreeItem("Remitente: "+mensaje.getRemitente()));
+        rootItem.getChildren().add(new TreeItem("Asunto: "+mensaje.getAsunto()));
+        rootItem.getChildren().add(new TreeItem("Fecha: "+mensaje.getFecha()));
     }
 
     public TreeItem<String>getRootItem(){
